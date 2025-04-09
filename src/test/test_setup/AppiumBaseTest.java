c646721c4c6b000ed69fc93afe101ebed1753424
NatPod2022 <glotova84@yandex.ru>
2025-04-09 13:13:22 +0200

    ref

--- src/test/test_setup/AppiumBaseTest.java
+++ src/test/test_setup/AppiumBaseTest.java
@@ -1,28 +1,21 @@
 package base;
 
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
 import java.net.URL;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import test.configuration.CapabilitiesProvider;
 
 public class AppiumBaseTest {
     protected AppiumDriver driver;
 
     @Before
     public void setup() throws Exception {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "and82");
-        capabilities.setCapability("platformVersion", "8.0");
-        capabilities.setCapability("appPackage", "com.novakid.student");
-        capabilities.setCapability("appActivity", "com.novakid.student.MainActivity");
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("app", "C:/Users/Natalia/OneDrive/Рабочий стол/JavaAppiumAutomation/JavaAppiumAutomation/apks/app-prod.apk");
-        capabilities.setCapability("newCommandTimeout", 300);
-        capabilities.setCapability("adbExecTimeout", 60000);
-
+        DesiredCapabilities capabilities = CapabilitiesProvider.getCapabilities();
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
 
