f9b5a538f8a4a8fab7a389dad56ab2f4c6f90bed
NatPod2022 <glotova84@yandex.ru>
2025-09-17 10:35:34 +0200

    -add 29 test

---
+++ src/test/tests/e2e/BookMyTrialLessonButtonTest.java
@@ -0,0 +1,55 @@
+package test.tests.e2e;
+
+import base.AppiumBaseTest;
+import org.junit.Test;
+
+import test.test_setup.BasePages;
+
+public class BookMyTrialLessonButtonTest extends AppiumBaseTest {
+
+    @Test
+    // Test Case #29
+    public void testBookMyTrialLessonButton() {
+        // Инициализация всех Page Object'ов через BasePages
+        BasePages pages = new BasePages();
+        pages.initPages(driver);
+
+        // Клик по кнопке "Allow", если она есть
+        pages.allowPermission.clickAllowIfVisible();
+
+        // Steps:
+
+
+        // Click on LogIn
+        pages.testPreLogin.getLoginButton().click();
+
+        // Click on LogInEmailPhoneButton
+        pages.testLogInPageFirstStep.getLogInEmailPhoneButton().click();
+
+        // Click on LoginWithPasswordButton
+        pages.testLogInStepByPhone.getLoginWithPasswordPhoneButton().click();
+
+        // Insert Email and Password
+        pages.testLogInStepByEmail.getLogInInputField().click();
+        pages.testLogInStepByEmail.getLogInInputField().sendKeys("np_test@mail.nv");
+
+        pages.testLoginWithPasswordEmail.getPasswordInputField().click();
+        pages.testLoginWithPasswordEmail.getPasswordInputField().sendKeys("12345678");
+
+
+        // Click on Sign in
+        pages.testLoginWithPasswordPhone.getSignInPhoneButton().click();
+
+        //Click on button
+
+        pages.testDashboardPage.getBookButton().click();
+
+        // Check all elements on Choose Teacher screen
+        pages.testChooseTeacherPage.getBackButton();
+        pages.testChooseTeacherPage.getChooseTeacherTitle();
+        pages.testChooseTeacherPage.getAvailableTeachersLabel();
+        pages.testChooseTeacherPage.getReadMoreButton();
+        pages.testChooseTeacherPage.getBookLessonButton();
+
+    }
+}
